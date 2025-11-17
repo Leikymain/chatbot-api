b9beabfef0c5918bee4dbb8435e5de2391deb34b
Leikymain <lagojorge24@gmail.com>
2025-11-17 22:30:07 +0100

    ADd depends

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
